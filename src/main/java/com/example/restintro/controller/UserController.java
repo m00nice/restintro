d9cdf56c65ba57b5a3579379293e308131e35798
m00ni <m00nice@live.com>
2022-09-02 09:19:44 +0200

    gr

--- src/main/java/com/example/restintro/controller/UserController.java
+++ src/main/java/com/example/restintro/controller/UserController.java
@@ -2,16 +2,15 @@ package com.example.restintro.controller;
 
 import com.example.restintro.model.User;
 import com.example.restintro.service.UserService;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 
@@ -34,12 +33,15 @@ public class UserController {
     public User getUser(@RequestBody User user){
         return userService.save(user);
     }
-/*
-    @PostMapping("/deleteUser")
-    public RequestEntity<User> deleteUser(){
 
+    @PostMapping("/deleteUser/{id}")
+    public Map<String, Boolean> deleteUser(@PathVariable(value = "id") Long userId)
+    throws ResourceNotFoundException{
+    User user = userService.findById(userId).orElseThrow() -> new ResourceNotFoundException("user not found");
+
+    userService.delete(user);
     }
-
+/*
     @PostMapping("/editUser")
     public RequestEntity<User> editUser(){
 
